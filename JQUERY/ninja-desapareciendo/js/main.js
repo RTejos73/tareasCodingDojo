4f0dd877b1437bf8174fcd078260df8ef7f1be17
Rene Tejos <rene.tejos@icloud.com>
2021-05-06 19:49:58 -0400

    tarea ninja-desapareciendo

---
+++ JQUERY/ninja-desapareciendo/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+    $('img').click(function () {
+        $(this).hide('linear');
+    })
+
+    $('#bt1').click(function () {
+        $('img').show();
+    });
+
+
+
+
+
+
+
+});
