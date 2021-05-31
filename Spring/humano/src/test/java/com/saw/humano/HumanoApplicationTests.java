c202c62eab14219f17dff1d2cbea7eda1666f504
Rene Tejos <rene.tejos@icloud.com>
2021-05-30 20:43:29 -0400

    tarea spring humano

---
+++ Spring/humano/src/test/java/com/saw/humano/HumanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saw.humano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
