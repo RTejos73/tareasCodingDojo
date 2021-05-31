0dcdb7934d22eb4924a8911c6471607b4f709d18
Rene Tejos <rene.tejos@icloud.com>
2021-05-30 20:24:21 -0400

    tarea spring enrutamiento

---
+++ Spring/enrutamiento/src/main/java/com/saw/enrutamiento/DojoController.java
@@ -0,0 +1,24 @@
+package com.saw.enrutamiento;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DojoController {
+
+	@RequestMapping("/{track}")
+	public String showLesson(@PathVariable("track") String track){
+		if(track.equals("dojo") ) {
+			return "¡El Dojo es increíble!";
+		}else if (track.equals("burbank-dojo")) {
+				return "El Dojo Burbank está localizado al sur de California";
+		}else if (track.equals("san-jose")) {
+			return "El Dojo SJ es el cuartel general";
+		}else
+		return "Variable Path " + track + " no encontrada";
+    }
+	
+	
+	
+}
